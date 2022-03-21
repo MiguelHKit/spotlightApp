a7e18712d711805e71cbcf9626d5742950fc0097
piktamine <49212598+piktamine@users.noreply.github.com>
2022-03-21 14:49:57 -0600

    V0.6

--- spotlightApp/ViewController.m
+++ spotlightApp/ViewController.m
@@ -21,9 +21,16 @@
 
 - (IBAction)focoSwitch:(UISwitch *)sender {
     if ([sender isOn]) {
+        //lo que hace si el foco se enciende
         self.focoPrendidoImage.hidden = NO;
+        self.focoPrendidoImage.alpha = 1;//en caso que quede con otro valor, volvemos a ponerlo sin transparencia
+        self.alphaSlider.enabled = YES;
+        self.alphaSlider.value = 1;//ponemos el slider a full
     } else {
+        //lo que hace si el foco se apaga
         self.focoPrendidoImage.hidden = YES;
+        self.alphaSlider.enabled = NO;
+        self.alphaSlider.value = 0;//ponemos el slider a 0
     }
 }
 - (IBAction)changeAlphaSlider:(UISlider *)sender {
